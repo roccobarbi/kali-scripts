b26356db26ed34be1a2c503a8668eec13bf9de54
Rocco Barbini <roccobarbi@gmail.com>
2021-03-27 04:40:59 -0400

    redundant line removed, the latest version of plink is already downloaded from the internet

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 mkdir webserver
 cp /usr/share/laudanum/php/php-reverse-shell.php ./webserver/
 cp /usr/share/windows-resources/binaries/nc.exe ./webserver/
-cp /usr/share/windows-resources/binaries/plink.exe ./webserver/
 sudo wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh -P ./webserver/
 sudo wget "https://github.com/andrew-d/static-binaries/blob/master/binaries/linux/x86_64/socat?raw=true" -O ./webserver/socat
 sudo wget "https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe" -O ./webserver/plink_64.exe
