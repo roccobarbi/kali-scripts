8af1c8cccaff677e47da4405013ce459fe4c8b5f
Rocco Barbini <roccobarbi@gmail.com>
2021-04-14 17:06:27 -0400

    nmap static file for linux added

--- project-folder.setup.sh
+++ project-folder.setup.sh
@@ -7,6 +7,7 @@ sudo wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awe
 sudo wget "https://github.com/andrew-d/static-binaries/blob/master/binaries/linux/x86_64/socat?raw=true" -O ./webserver/socat
 sudo wget "https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe" -O ./webserver/plink_64.exe
 sudo wget "https://the.earth.li/~sgtatham/putty/latest/w32/plink.exe" -O ./webserver/plink_32.exe
+sudo wget "https://github.com/andrew-d/static-binaries/blob/master/binaries/linux/x86_64/nmap?raw=true" -O ./webserver/nmap-linux-static
 echo "#!/bin/bash" >> server.sh
 echo "python -m http.server -d webserver 8080" >> server.sh
 chmod 755 server.sh
