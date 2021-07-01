797c4c42ff4aa90bbd40e720afb052794ca4d10b
Rocco Barbini <roccobarbi@gmail.com>
2021-07-01 17:50:38 -0400

    xplatform wordlist added

--- project-folder.setup.sh
+++ project-folder.setup.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 mkdir webserver
+mkdir wordlists
 cp /usr/share/laudanum/php/php-reverse-shell.php ./webserver/
 cp /usr/share/windows-resources/binaries/nc.exe ./webserver/
 sudo wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh -P ./webserver/
@@ -8,6 +9,7 @@ sudo wget "https://github.com/andrew-d/static-binaries/blob/master/binaries/linu
 sudo wget "https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe" -O ./webserver/plink_64.exe
 sudo wget "https://the.earth.li/~sgtatham/putty/latest/w32/plink.exe" -O ./webserver/plink_32.exe
 sudo wget "https://github.com/andrew-d/static-binaries/blob/master/binaries/linux/x86_64/nmap?raw=true" -O ./webserver/nmap-linux-static
+sudo wget "https://raw.githubusercontent.com/fuzzdb-project/fuzzdb/master/attack/sql-injection/detect/xplatform.txt" -O ./wordlists/xplatform.txt
 echo "#!/bin/bash" >> server.sh
 echo "python -m http.server -d webserver 8080" >> server.sh
 chmod 755 server.sh
